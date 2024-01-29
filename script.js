461bc0307b5ed9e1adc4da950908ff3a5a2c1295
gurleenkaur1108 <76990139+gurleenkaur1108@users.noreply.github.com>
2024-01-29 15:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+//your code here
 function firstNonRepeatedChar(str) {
- // Write your code here
+  let charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]]) {
+      charCount[str[i]]++;
+    } else {
+      charCount[str[i]] = 1;
+    }
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]] === 1) {
+      return str[i];
+    }
+  }
+  return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input)); //"abcabcdbca" - d
